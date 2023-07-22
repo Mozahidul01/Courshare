e8279f103bdff12ff4cd02703cd3bd5248a16d69
mozahidul01 <mozo.postbox@gmail.com>
2023-07-23 03:29:15 +0600

    some minor bug fix

--- src/Layouts/PrivateRoute.js
+++ src/Layouts/PrivateRoute.js
@@ -1,13 +1,12 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../contexts/UserContex";
-
+import { AuthContext } from "../contexts/UserContext";
 export default function PrivateRoute({ children }) {
 	const { user, loading } = useContext(AuthContext);
 
 	if (loading) {
-		return <button className='btn btn-lg btn-ghost btn-square loading' />;
-	} else if (!loading && user.uid) {
+		return <div className='btn btn-lg btn-ghost btn-square loading' />
+	} else if (user && user.uid) {
 		return children;
 	} else {
 		return <Navigate to='/log-in' />;
